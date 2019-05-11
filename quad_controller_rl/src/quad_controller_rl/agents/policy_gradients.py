6c7968d9ca9c369a955c287ebb0e62025cc3e7b9
SimonLee <lixiaomin_235@163.com>
2019-05-11 11:01:49 +0800

    add debug info for code reading

--- quad_controller_rl/src/quad_controller_rl/agents/policy_gradients.py
+++ quad_controller_rl/src/quad_controller_rl/agents/policy_gradients.py
@@ -10,14 +10,23 @@ class DDPG(BaseAgent):
         # Task (environment) information
         self.task = task  # should contain observation_space and action_space
         self.state_size = np.prod(self.task.observation_space.shape)
+        # self.state_size = 3  # position only
         self.state_range = self.task.observation_space.high - self.task.observation_space.low
         self.action_size = np.prod(self.task.action_space.shape)
+        # self.action_size = 3  # force only
         self.action_range = self.task.action_space.high - self.task.action_space.low
+        print("Original spaces: {}, {}\nConstrained spaces: {}, {}".format(
+            self.task.observation_space.shape, self.task.action_space.shape,
+            self.state_size, self.action_size))
+        print("observation_space: high={}, low={}, range={}\naction_space: high={}, low={}, range={}".format(
+            self.task.observation_space.high, self.task.observation_space.low, self.state_range,
+            self.task.action_space.high, self.task.action_space.low, self.action_range))
 
         # Policy parameters
         self.w = np.random.normal(
             size=(self.state_size, self.action_size),  # weights for simple linear policy: state_space x action_space
             scale=(self.action_range / (2 * self.state_size)).reshape(1, -1))  # start producing actions in a decent range
+        print("init self.w => {}".format(self.w))
 
         # Score tracker and learning parameters
         self.best_w = None
@@ -33,10 +42,23 @@ class DDPG(BaseAgent):
         self.total_reward = 0.0
         self.count = 0
 
+    def preprocess_state(self, state):
+        """Reduce state to relevant dimensions."""
+        return state[0:3]  # position only
+
+    def postprocess_action(self, action):
+        """Return complete action vector."""
+        complete_action = np.zeros(self.task.action_space.shape)
+        complete_action[0:3] = action  # linear force only
+        return complete_action
+
     def step(self, state, reward, done):
+        # state = self.preprocess_state(state)
+
         # Transform state vector
         state = (state - self.task.observation_space.low) / self.state_range  # scale to [0.0, 1.0]
         state = state.reshape(1, -1)  # convert to row vector
+        print("state => {}".format(state))
 
         # Choose an action
         action = self.act(state)
@@ -54,11 +76,12 @@ class DDPG(BaseAgent):
         self.last_state = state
         self.last_action = action
         return action
+        # return self.postprocess_action(action)
 
     def act(self, state):
         # Choose action based on given state and policy
         action = np.dot(state, self.w)  # simple linear policy
-        #print(action)  # [debug: action vector]
+        print("action => {}".format(action))  # [debug: action vector]
         return action
 
     def learn(self):
@@ -75,4 +98,4 @@ class DDPG(BaseAgent):
         self.w = self.w + self.noise_scale * np.random.normal(size=self.w.shape)  # equal noise in all directions
         print("RandomPolicySearch.learn(): t = {:4d}, score = {:7.3f} (best = {:7.3f}), noise_scale = {}".format(
                 self.count, score, self.best_score, self.noise_scale))  # [debug]
-        #print(self.w)  # [debug: policy parameters]
+        print("self.w => {}".format(self.w))  # [debug: policy parameters]
