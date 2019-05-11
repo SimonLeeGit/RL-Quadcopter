6d6895f00901e3dbf901343207b99cd9a0898c48
SimonLee <lixiaomin_235@163.com>
2019-05-10 23:28:50 -0700

    set observation & action shape to 3

--- quad_controller_rl/src/quad_controller_rl/agents/policy_gradients.py
+++ quad_controller_rl/src/quad_controller_rl/agents/policy_gradients.py
@@ -9,24 +9,22 @@ class DDPG(BaseAgent):
     def __init__(self, task):
         # Task (environment) information
         self.task = task  # should contain observation_space and action_space
-        self.state_size = np.prod(self.task.observation_space.shape)
-        # self.state_size = 3  # position only
-        self.state_range = self.task.observation_space.high - self.task.observation_space.low
-        self.action_size = np.prod(self.task.action_space.shape)
-        # self.action_size = 3  # force only
-        self.action_range = self.task.action_space.high - self.task.action_space.low
+        self.state_size = 3  # position only
+        self.state_high = np.split(self.task.observation_space.high, [self.state_size], 0)[0]
+        self.state_low = np.split(self.task.observation_space.low, [self.state_size], 0)[0]
+        self.state_range = self.state_high - self.state_low
+        self.action_size = 3  # force only
+        self.action_high = np.split(self.task.action_space.high, [self.action_size], 0)[0]
+        self.action_low = np.split(self.task.action_space.low, [self.action_size], 0)[0]
+        self.action_range = self.action_high - self.action_low
         print("Original spaces: {}, {}\nConstrained spaces: {}, {}".format(
             self.task.observation_space.shape, self.task.action_space.shape,
             self.state_size, self.action_size))
-        print("observation_space: high={}, low={}, range={}\naction_space: high={}, low={}, range={}".format(
-            self.task.observation_space.high, self.task.observation_space.low, self.state_range,
-            self.task.action_space.high, self.task.action_space.low, self.action_range))
 
         # Policy parameters
         self.w = np.random.normal(
             size=(self.state_size, self.action_size),  # weights for simple linear policy: state_space x action_space
             scale=(self.action_range / (2 * self.state_size)).reshape(1, -1))  # start producing actions in a decent range
-        print("init self.w => {}".format(self.w))
 
         # Score tracker and learning parameters
         self.best_w = None
@@ -53,12 +51,12 @@ class DDPG(BaseAgent):
         return complete_action
 
     def step(self, state, reward, done):
-        # state = self.preprocess_state(state)
+        state = self.preprocess_state(state)
 
         # Transform state vector
-        state = (state - self.task.observation_space.low) / self.state_range  # scale to [0.0, 1.0]
+        state = (state - self.state_low) / self.state_range  # scale to [0.0, 1.0]
         state = state.reshape(1, -1)  # convert to row vector
-        print("state => {}".format(state))
+        # print("state => {}".format(state))
 
         # Choose an action
         action = self.act(state)
@@ -75,13 +73,13 @@ class DDPG(BaseAgent):
 
         self.last_state = state
         self.last_action = action
-        return action
-        # return self.postprocess_action(action)
+        # return action
+        return self.postprocess_action(action)
 
     def act(self, state):
         # Choose action based on given state and policy
         action = np.dot(state, self.w)  # simple linear policy
-        print("action => {}".format(action))  # [debug: action vector]
+        # print("action => {}".format(action))  # [debug: action vector]
         return action
 
     def learn(self):
@@ -98,4 +96,4 @@ class DDPG(BaseAgent):
         self.w = self.w + self.noise_scale * np.random.normal(size=self.w.shape)  # equal noise in all directions
         print("RandomPolicySearch.learn(): t = {:4d}, score = {:7.3f} (best = {:7.3f}), noise_scale = {}".format(
                 self.count, score, self.best_score, self.noise_scale))  # [debug]
-        print("self.w => {}".format(self.w))  # [debug: policy parameters]
+        # print("self.w => {}".format(self.w))  # [debug: policy parameters]
